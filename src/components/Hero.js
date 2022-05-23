81d65af7b63e20d0c7629fde350199dd37aec82f
Kep <KepCreations@gmail.com>
2022-05-23 18:10:07 -0400

    Set up grid system

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,34 @@
+import { Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 import './Hero.css';
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <h1>Kep</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
-    </div>
+    <Box sx={{ marginTop: '72px', marginBottom: '72px' }}>
+      <Grid container spacing={2}>
+        <Grid item md={4} sm={12}>
+          <Typography
+            variant="h1"
+            sx={{ fontFamily: 'Gruppo', fontSize: '144px' }}
+          >
+            Kep
+          </Typography>
+        </Grid>
+
+        <Grid item md={8} sm={12}>
+          <Typography
+            variant="p"
+            sx={{ fontFamily: 'Gruppo', fontSize: '36px' }}
+          >
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 import tapntableScreenshot from '../static/tapntableScreenshot.jpg';
+import { Box, Grid, Typography } from '@mui/material';
 
 const ProjectList = () => {
   return (
-    <div>
-      <ProjectCard
-        title="Tapntable"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        image={tapntableScreenshot}
-      />
-      <ProjectCard
-        title="TuttiTracks"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        image={tapntableScreenshot}
-      />
-      <ProjectCard
-        title="Casting Agency"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        image={tapntableScreenshot}
-      />
-      <ProjectCard
-        title="Lorem Ipsum"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        image={tapntableScreenshot}
-      />
-    </div>
+    <Box>
+      <Typography variant="h2">Projects</Typography>
+      <Grid container spacing={2}>
+        <Grid item md={6}>
+          <ProjectCard
+            title="Tapntable"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            image={tapntableScreenshot}
+          />
+        </Grid>
+
+        <Grid item md={6}>
+          <ProjectCard
+            title="TuttiTracks"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            image={tapntableScreenshot}
+          />
+        </Grid>
+
+        <Grid item md={6}>
+          <ProjectCard
+            title="Casting Agency"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            image={tapntableScreenshot}
+          />
+        </Grid>
+
+        <Grid item md={6}>
+          <ProjectCard
+            title="Lorem Ipsum"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            image={tapntableScreenshot}
+          />
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
