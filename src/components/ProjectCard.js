45f6a20548118a61c053621633861485ca89bd46
Kep <KepCreations@gmail.com>
2022-05-23 14:59:48 -0400

    Create testImage

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import testImage from '../static/testImage.jpg';
 
-const ProjectCard = ({ title, description, image }) => {
+export default function ProjectCard({ title, description, image }) {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia component="img" height="140" image={testImage} alt={title} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Demo</Button>
+        <Button size="small">Github</Button>
+      </CardActions>
+    </Card>
   );
-};
-
-export default ProjectCard;
+}
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
+import testImage from '../static/testImage.jpg';
 
 const ProjectList = () => {
   return (
     <div>
-      <ProjectCard title="Tapntable" description="" image="" />
-      <ProjectCard title="TuttiTracks" description="" image="" />
-      <ProjectCard title="Casting Agency" description="" image="" />
-      <ProjectCard title="Lorem Ipsum" description="" image="" />
+      <ProjectCard title="Tapntable" description="" image={testImage} />
+      <ProjectCard title="TuttiTracks" description="" image={testImage} />
+      <ProjectCard title="Casting Agency" description="" image={testImage} />
+      <ProjectCard title="Lorem Ipsum" description="" image={testImage} />
     </div>
   );
 };
