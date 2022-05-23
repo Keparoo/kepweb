c8541c82488ccc0416979a45736b7447d9fcfb7b
Kep <KepCreations@gmail.com>
2022-05-23 14:12:42 -0400

    Update Avatar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -129,7 +129,7 @@ const Navbar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Kep">K</Avatar>
               </IconButton>
             </Tooltip>
             <Menu
