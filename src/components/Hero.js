806522a5578ee5476ffa96bfd4146720211e2b50
Kep <KepCreations@gmail.com>
2022-05-23 17:27:53 -0400

    Add custom fonts

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Hero.css';
 
 const Hero = () => {
   return (
-    <div>
+    <div className="hero">
       <h1>Kep</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
     </div>
   );
 };
