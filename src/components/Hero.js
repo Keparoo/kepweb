cc49781247456598085bfb14bf7f6854a12563dc
Kep <KepCreations@gmail.com>
2022-05-26 22:15:22 -0400

    Update hero font to asap

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,17 +9,14 @@ const Hero = () => {
         <Grid item md={4} sm={12}>
           <Typography
             variant="h1"
-            sx={{ fontFamily: 'Gruppo', fontSize: '144px' }}
+            sx={{ fontFamily: 'Asap', fontSize: '144px' }}
           >
             Kep
           </Typography>
         </Grid>
 
         <Grid item md={8} sm={12}>
-          <Typography
-            variant="p"
-            sx={{ fontFamily: 'Gruppo', fontSize: '36px' }}
-          >
+          <Typography variant="p" sx={{ fontSize: '36px' }}>
             <br />
             I'm a full stack web developer living in Cape Cod, Massachusetts.
           </Typography>
@@ -30,3 +27,5 @@ const Hero = () => {
 };
 
 export default Hero;
+
+// sx={{ fontFamily: 'Gruppo', fontSize: '36px' }}
