a67682077e4d43ebb946cc99c47841a10680d866
Kep <KepCreations@gmail.com>
2022-06-27 16:36:28 -0400

    Add demo instructions to project cards

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -29,7 +29,10 @@ const ProjectList = () => {
                 <strong>Frontend:</strong> Single-page React-Redux user
                 interface managing orders, checks, payments, end-of-day
                 reconciliation, sales reports, timeclock, and database
-                management
+                management.<br />
+                <br />
+                <strong>Note:</strong>The deployed site will allow a user to log
+                in with a demo-pin: 12345 to try out the app.
               </span>
             }
             image={tapntable}
@@ -49,7 +52,13 @@ const ProjectList = () => {
                 uploaded to the user's Spotify account.<br />
                 <strong>Backend</strong>: Python with a PostgreSQL database, a
                 RESTful API, Oauth authorization, and unittest test suite.<br />
-                <strong>Frontend</strong>: Python, Flask, and Jinja2 templates.
+                <strong>Frontend</strong>: Python, Flask, and Jinja2 templates.{' '}
+                <br />
+                <br />
+                <strong>Note:</strong> In order to use the app, if the user is
+                not currently logged into their Spotify account they will be
+                directed to login or signup using OAuth. A user may then create
+                a Tuttitracks account to create and manage playlists.
               </span>
             }
             image={tuttitracks}
@@ -67,7 +76,12 @@ const ProjectList = () => {
                 agency<br />
                 <strong>Backend</strong>: PostgreSQL database with a RESTful API
                 using Python and Flask, role-based access control authorization
-                utilizing Auth0, test suite, and deployed to Heroku.
+                utilizing Auth0, test suite, and deployed to Heroku. <br />
+                <br />
+                <strong>Note:</strong>The deployment link is below but this is
+                an API <em>only</em> that upon login, returns a JWT with the
+                appropriate role-based auth to the screen to access the API.
+                Currently there is no interactive front-end written.
               </span>
             }
             image={castingAgency}
