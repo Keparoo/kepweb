96e3231da4bb9de8a1810e64e6d55cbe7918408c
Kep <KepCreations@gmail.com>
2022-07-13 17:53:12 -0400

    Fix next errors in tapntable page

--- pages/tapntable.js
+++ pages/tapntable.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Typography } from '@mui/material';
-import Navbar from '../components/Navbar';
+import Navbar from '../src/Navbar';
 
 const Tapntable = () => {
   return (
