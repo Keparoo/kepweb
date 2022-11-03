ba365bb1f8ea7077383c394ace97b4b510a38ddf
Kep <KepCreations@gmail.com>
2022-11-02 23:00:27 -0400

    Add call to action button to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 import computerImage from '../static/pixeltrue-web-development-1.svg';
@@ -20,11 +20,20 @@ const Hero = () => {
             <Typography
               className="hero-subhead"
               variant="p"
-              sx={{ fontSize: '2.25rem' }}
+              sx={{ display: 'block', fontSize: '2.25rem' }}
             >
               <br />
               I'm a full-stack web developer with a passion for challenges.
             </Typography>
+            <Button
+              variant="contained"
+              href="#contact"
+              size="large"
+              fullWidth={false}
+              sx={{ marginTop: '3em' }}
+            >
+              Contact Me
+            </Button>
           </Grid>
 
           <Grid item md={6} sm={12}>
