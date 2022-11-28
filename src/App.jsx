176b924b382c86e2b8c85fd7b26a8d01e0bdc19e
Kep <KepCreations@gmail.com>
2022-11-28 15:00:48 -0500

    Remove Box and MUI Footer components. Replace with Grid CSS and vanilla HTML

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero/Hero';
 import ProjectList from './components/Projects/ProjectList';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 
 import './App.css';
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import HomeIcon from '@mui/icons-material/Home';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InfoIcon from '@mui/icons-material/Info';
+import { Tooltip } from '@mui/material';
+
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <section className={styles.footer}>
+      <nav className={styles.footerNavigation}>
+        <a href="#hero" aria-label="Home">
+          <Tooltip title="Go Home" enterTouchDelay={0}>
+            <HomeIcon size={30} className={styles.footerIcon} />
+          </Tooltip>
+        </a>
+
+        <a href="#about" aria-label="About me">
+          <Tooltip title="Go to About Me" enterTouchDelay={0}>
+            <InfoIcon size={30} className={styles.footerIcon} />
+          </Tooltip>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/kep-kaeppeler/"
+          target="_blank"
+          rel="noopener"
+          aria-label="LinkedIn profile"
+        >
+          <Tooltip title="Open my LinkedIn page" enterTouchDelay={0}>
+            <LinkedInIcon size={30} className={styles.footerIcon} />
+          </Tooltip>
+        </a>
+
+        <a
+          href="https://github.com/Keparoo"
+          target="_blank"
+          rel="noopener"
+          aria-label="GitHub repository"
+        >
+          <Tooltip title="Open my GitHub page" enterTouchDelay={0}>
+            <GitHubIcon size={30} className={styles.footerIcon} />
+          </Tooltip>
+        </a>
+      </nav>
+
+      <p className={styles.copyright}>&copy; 2022 Kepweb.dev</p>
+    </section>
+  );
+}
