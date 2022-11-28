4a2b9b6093abccae274b84a993c611251f116d4a
Kep <KepCreations@gmail.com>
2022-11-28 13:51:45 -0500

    Replace Grid components with CSS Grid

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,35 +2,33 @@ import React from 'react';
 
 import computerImage from '../../static/pixeltrue-web-development-1.svg';
 
-import { Button, Grid } from '@mui/material';
+import { Button } from '@mui/material';
 
 import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <section id="hero" className={styles.hero}>
-      <Grid container spacing={2}>
-        <Grid item md={6} sm={12}>
-          <h1 className={styles.heroHeader}>Kep</h1>
-          <p className={styles.heroSubhead}>
-            <br />
-            I'm a full-stack web developer with a passion for challenges.
-          </p>
-          <Button
-            variant="contained"
-            href="#contact"
-            size="large"
-            fullWidth={false}
-            sx={{ marginTop: '3em' }}
-          >
-            Contact Me
-          </Button>
-        </Grid>
+    <section id="hero" className={`${styles.hero} ${styles.columns}`}>
+      <div>
+        <h1 className={styles.heroHeader}>Kep</h1>
+        <p className={styles.heroSubhead}>
+          <br />
+          I'm a full-stack web developer with a passion for challenges.
+        </p>
+        <Button
+          variant="contained"
+          href="#contact"
+          size="large"
+          fullWidth={false}
+          sx={{ marginTop: '3em' }}
+        >
+          Contact Me
+        </Button>
+      </div>
 
-        <Grid item md={6} sm={12}>
-          <img className="computer-image" src={computerImage} />
-        </Grid>
-      </Grid>
+      <div>
+        <img className="computer-image" src={computerImage} />
+      </div>
     </section>
   );
 };
