68d5c9f9ec62cd9af0d379c46a5e509d515fe010
Kep <KepCreations@gmail.com>
2023-01-17 19:57:04 -0500

    Continue tooltip work

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -3,6 +3,8 @@ import React from 'react';
 import styles from './Tooltip.module.css';
 
 /* Tooltip Component */
+
+/* Figure out how to affect a component that is wrapped */
 export const Tooltip = () => {
   return (
     <div>
