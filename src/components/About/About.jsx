ccb1d83cf341a7c74bb3de12b0e9710f6a4e82c2
Kep <KepCreations@gmail.com>
2023-02-26 19:54:52 -0500

    Update Mastodon About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -207,7 +207,7 @@ const Fediverse = () => {
       </p>
       <p>
         <strong>
-          I am a Patreon supporter of my Mastodon instance,{' '}
+          I am a Patreon supporter of my Mastodon instance:{' '}
           <a
             className="link"
             rel="noopener"
